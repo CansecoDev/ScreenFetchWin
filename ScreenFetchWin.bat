84bd77bbb54c1e73272b353170e68c2818c32e25
Segovia <35817798+SegoCode@users.noreply.github.com>
2018-04-02 21:56:01 +0200

    Delete year in Software License

--- ScreenFetchWin.bat
+++ ScreenFetchWin.bat
@@ -3,7 +3,7 @@ mode con:cols=110 lines=18
 title ScreenfetchWin
 
 echo loading...
-rem :: ScreenFetchWin Software License (c) 2018
+rem :: ScreenFetchWin Software License
 rem :: 
 rem :: It's free to use for everyone, always, in all environments.
 rem :: Under no circumstances can our software be monetized for commercial or any other purposes.
