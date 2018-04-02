c7cf91900714bda0d2d80fb690aee897ab1a0903
Segovia <35817798+SegoCode@users.noreply.github.com>
2018-04-02 21:55:37 +0200

    Add funny software license

--- ScreenFetchWin.bat
+++ ScreenFetchWin.bat
@@ -1,3 +1,14 @@
 @echo off
 mode con:cols=110 lines=18
 title ScreenfetchWin
+
+echo loading...
+rem :: ScreenFetchWin Software License (c) 2018
+rem :: 
+rem :: It's free to use for everyone, always, in all environments.
+rem :: Under no circumstances can our software be monetized for commercial or any other purposes.
+rem ::
+rem :: ScreenFetchWin comes with NO WARRANTY. We are not responsible for any damages.
+rem :: You can edit, break, and modify the program for your own personal and academic use.
+rem :: You cannot sell, modify and/or repackage our software and redistribute it.
+rem :: In short, don't be an asshole and you're good.
