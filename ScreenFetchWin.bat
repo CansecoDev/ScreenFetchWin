337624c2aad738c9a86f612e39fd14bbabedb34e
Segovia <35817798+SegoCode@users.noreply.github.com>
2018-04-03 18:52:03 +0200

    Delete REM and Add Version detect

--- ScreenFetchWin.bat
+++ ScreenFetchWin.bat
@@ -3,12 +3,18 @@ mode con:cols=110 lines=18
 title ScreenfetchWin
 
 echo loading...
-rem :: ScreenFetchWin Software License
-rem :: 
-rem :: It's free to use for everyone, always, in all environments.
-rem :: Under no circumstances can our software be monetized for commercial or any other purposes.
-rem ::
-rem :: ScreenFetchWin comes with NO WARRANTY. We are not responsible for any damages.
-rem :: You can edit, break, and modify the program for your own personal and academic use.
-rem :: You cannot sell, modify and/or repackage our software and redistribute it.
-rem :: In short, don't be an asshole and you're good.
+ :: ScreenFetchWin Software License
+ :: 
+ :: It's free to use for everyone, always, in all environments.
+ :: Under no circumstances can our software be monetized for commercial or any other purposes.
+ ::
+ :: ScreenFetchWin comes with NO WARRANTY. We are not responsible for any damages.
+ :: You can edit, break, and modify the program for your own personal and academic use.
+ :: You cannot sell, modify and/or repackage our software and redistribute it.
+ :: In short, don't be an asshole and you're good.
+
+:: Version
+for /f "tokens=4-5 delims=. " %%i in ('ver') do set VERSION=%%i.%%j
+if "%version%" == "10.0" set version=Windows 10
+if "%version%" == "6.3" set version=Windows 8.1
+if "%version%" == "6.2" set version=Windows 8
